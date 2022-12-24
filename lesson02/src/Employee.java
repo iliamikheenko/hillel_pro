1a5672287f35027f5227e663eabbbeb117f3c0f7
mouceu237 <ilyamikheenko@gmail.com>
2022-12-24 20:52:27 +0400

    update file Employee

--- lesson02/src/Employee.java
+++ lesson02/src/Employee.java
@@ -5,7 +5,7 @@ public class Employee {
     private String phoneNumber;
     private int age;
 
-    public Employeer(String fullName, String position, String email, String phoneNumber, int age) {
+    public Employee(String fullName, String position, String email, String phoneNumber, int age) {
         this.fullName = fullName;
         this.position = position;
         this.email = email;
