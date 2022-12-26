564e8117f053fb78390479d3ffcea2cc2efbc283
mouceu237 <ilyamikheenko@gmail.com>
2022-12-26 14:37:44 +0400

    updated all files

--- lesson03/src/Animal.java
+++ lesson03/src/Animal.java
@@ -1,2 +1,18 @@
-public class Animal {
+public abstract class Animal {
+    private String name;
+    private static int amountOfAnimals;
+    public Animal(String name) {
+        this.name = name;
+        amountOfAnimals++;
+    }
+    public static int getAmountOfUnits() {
+        return amountOfAnimals;
+    }
+    String getName() {
+        return name;
+    }
+    public void run(int dist){}
+    public void swim(int dist){}
+
+
 }
--- lesson03/src/Dog.java
+++ lesson03/src/Dog.java
@@ -1,2 +1,30 @@
 public class Dog extends Animal{
+    private static int amountOfDogs;
+    public Dog(String name) {
+        super(name);
+        amountOfDogs++;
+    }
+    public static int getAmountOfUnits() {
+        return amountOfDogs;
+    }
+    @Override
+    public void run(int dist){
+        if (dist < 0){
+            System.out.println("Invalid argument.\n");
+        }else if (dist <= 500){
+            System.out.printf("%s ran %dm.\n", this.getName(), dist);
+        } else {
+            System.out.printf("%s can't run more than 500m.\n", this.getName());
+        }
+    }
+    @Override
+    public void swim(int dist){
+        if (dist < 0){
+            System.out.println("Invalid argument.\n");
+        }else if (dist <= 10){
+            System.out.printf("%s swam %dм.\n", this.getName(), dist);
+        } else {
+            System.out.printf("%s can't' swim more than 10m.\n", this.getName());
+        }
+    }
 }
