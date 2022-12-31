2ba67d858da54fb03d8132c0a0219aa8526e6b7f
mouceu237 <ilyamikheenko@gmail.com>
2022-12-31 14:42:16 +0400

    added lesson04 branch from basic

---
+++ lesson04/src/phonebook/PhoneBook.java
@@ -0,0 +1,30 @@
+package src.phonebook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneBook {
+    private final ArrayList<Record> phoneBook = new ArrayList<>();
+    public void add(String name, String number){
+        if (name == null || number == null)
+            throw new IllegalArgumentException("name and number can't be null");
+        phoneBook.add(new Record(name,number));
+    }
+    public Record find(String name){
+        for (Record record : phoneBook) {
+            if (record.getName().equalsIgnoreCase(name)) {
+                return record;
+            }
+        }
+        return null;
+    }
+    public List<Record> findAll(String name){
+        List<Record> records = new ArrayList<>();
+        for (Record record : phoneBook) {
+            if (name.equalsIgnoreCase(record.getName())) {
+                records.add(record);
+            }
+        }
+        return records;
+    }
+}
