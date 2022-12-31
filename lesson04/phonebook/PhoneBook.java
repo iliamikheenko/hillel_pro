efc48ae2384496bf39a0393906cb337ffbf026d0
mouceu237 <ilyamikheenko@gmail.com>
2022-12-31 13:25:45 +0400

    updated

--- lesson04/phonebook/PhoneBook.java
+++ lesson04/phonebook/PhoneBook.java
@@ -1,6 +1,8 @@
 package hillel_pro.lesson04.phonebook;
 
 import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneBook {
     private final ArrayList<Record> phoneBook = new ArrayList<>();
     public void add(String name, String number){
@@ -12,20 +14,19 @@ public class PhoneBook {
     }
     public Record find(String name){
         for (Record record : phoneBook) {
-            if (name.equalsIgnoreCase(record.getName())) {
+            if (record.getName().equalsIgnoreCase(name)) {
                 return record;
             }
         }
         return null;
     }
-    public void findAll(String name){
-        ArrayList<String> allNumbers = new ArrayList<>();
+    public List<Record> findAll(String name){
+        List<Record> records = new ArrayList<>();
         for (Record record : phoneBook) {
             if (name.equalsIgnoreCase(record.getName())) {
-                allNumbers.add(record.getPhone());
+                records.add(record);
             }
         }
-        System.out.println("name: " + name +
-                           ", phones: " + allNumbers);
+        return records;
     }
 }
