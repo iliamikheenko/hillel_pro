0e6b7c3ff1bd59b719df9ce9eabe5d411a612a2e
mouceu237 <ilyamikheenko@gmail.com>
2023-02-10 17:47:39 +0400

    divided method textToWordCounter into textToLines and linesToWordCounter, edited constructor

--- lesson07/src/StatisticReader.java
+++ lesson07/src/StatisticReader.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class StatisticReader {
@@ -15,20 +12,29 @@ public class StatisticReader {
     private final Pattern PATTERN_WORD = Pattern.compile("[^a-zA-Z']");
 
     public StatisticReader(File book) {
-        this.textToWordCounter(book);
+        var lines = this.textToLines(book);
+        this.linesToWordCounter(lines);
     }
-    private void textToWordCounter(File book){
-        try (BufferedReader inputFile = new BufferedReader(new FileReader(book))){
+    private List<String> textToLines(File book) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader inputFile = new BufferedReader(new FileReader(book))) {
             String line;
-            while ((line = inputFile.readLine()) != null){
-                String[] words = PATTERN_WORD.split(line.trim().replaceAll("\\s+"," "));
-                for (String word : words) {
-                    word = word.toLowerCase();
-                    wordCounter.put(word,wordCounter.getOrDefault(word,0)+1);
-                }
+            while ((line = inputFile.readLine()) != null) {
+                lines.add(line);
             }
         } catch (IOException e) {
             System.out.println("Failed reading the file with text. Try again.");
+        }
+        return lines;
+    }
+
+    private void linesToWordCounter(List<String> lines){
+        for (String line : lines) {
+            String[] words = PATTERN_WORD.split(line.trim().replaceAll("\\s+", " "));
+            for (String word : words) {
+                word = word.toLowerCase();
+                wordCounter.put(word, wordCounter.getOrDefault(word, 0) + 1);
+            }
         }
     }
 
