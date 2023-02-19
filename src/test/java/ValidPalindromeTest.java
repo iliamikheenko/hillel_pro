669db8a9c9f9df794d6ef97af33a5bdb1b0bbb87
mouceu237 <ilyamikheenko@gmail.com>
2023-02-19 14:30:47 +0400

    changed checker var name to validator

--- src/test/java/ValidPalindromeTest.java
+++ src/test/java/ValidPalindromeTest.java
@@ -6,55 +6,55 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ValidPalindromeTest {
-    ValidPalindrome checker;
+    ValidPalindrome validator;
     @Before
     public void init(){
-        checker = new ValidPalindrome();
+        validator = new ValidPalindrome();
     }
     @Test
     public void isPalindrome_WithEmptyString_Test() {
         String text = "";
-        assertTrue(checker.isPalindrome(text));
+        assertTrue(validator.isPalindrome(text));
     }
     @Test
     public void isPalindrome_WithOneLetter_Test() {
         String text = "A";
-        assertTrue(checker.isPalindrome(text));
+        assertTrue(validator.isPalindrome(text));
     }
     @Test
     public void isPalindrome_WithOneSpace_Test() {
         String text = " ";
-        assertTrue(checker.isPalindrome(text));
+        assertTrue(validator.isPalindrome(text));
     }
 
     @Test
     public void isPalindrome_WithOnlySpaces_Test() {
         String text = "        ";
-        assertTrue(checker.isPalindrome(text));
+        assertTrue(validator.isPalindrome(text));
     }
     @Test
     public void isPalindrome_WithOnlyNotLetters_Test() {
         String text = ">#$!%@^$#!@!";
-        assertTrue(checker.isPalindrome(text));
+        assertTrue(validator.isPalindrome(text));
     }
     @Test
     public void isPalindrome_WithNumbers_Test() {
         String text = "1234321";
-        assertTrue(checker.isPalindrome(text));
+        assertTrue(validator.isPalindrome(text));
     }
     @Test
     public void isPalindrome_WithPolindromeText_Test() {
         String text = "a man, a plan, a canal: panama";
-        assertTrue(checker.isPalindrome(text));
+        assertTrue(validator.isPalindrome(text));
     }
     @Test
     public void isPalindrome_WithNotPolindromeText_Test() {
         String text = "race, a car";
-        assertFalse(checker.isPalindrome(text));
+        assertFalse(validator.isPalindrome(text));
     }
     @Test
     public void isPalindrome_WithMixedTextCases_Test() {
         String text = "anotherREHTONA";
-        assertTrue(checker.isPalindrome(text));
+        assertTrue(validator.isPalindrome(text));
     }
 }
